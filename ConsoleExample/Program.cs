4169cdeb878319c48ea977ce89602f9e26c20a03
Nikita Chekun <chekun2004@gmail.com>
2022-07-08 17:26:24 +0300

    Remove main example.

--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using CSharp.Extensions.Security.Cryptography;
-
-namespace ConsoleExample;
+﻿namespace ConsoleExample;
 
 internal static class Program
 {
     private static void Main()
     {
-        // Example
         
-        string str = Console.ReadLine() ?? ""; // string entered by the user
-
-        CryptographicHashString hashString = str; // default hash
-
-        string dbStr = hashString; // default hash e.g. from a database
-        
-        Console.WriteLine(dbStr & hashString); // comparison output
-        Console.WriteLine(hashString & dbStr); // comparison output
-        
-        // analogue of dbStr & hashString is CryptographicHashString.Compare(dbStr, hashString);
     }
 }
